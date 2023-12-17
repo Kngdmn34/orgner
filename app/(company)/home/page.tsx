fa85ff0e84d46aed6af49b1f3b5ec1d1d5fe26bb
Anas Alaoui <96668023+Kngdmn34@users.noreply.github.com>
2023-12-17 19:52:42 +0100

    Update page.tsx

--- app/(company)/home/page.tsx
+++ app/(company)/home/page.tsx
@@ -6,7 +6,7 @@ const Home = () => {
             <div className='flex flex-col justify-start mx-10 mt-16 '>
                 <h3 className='text-tiny text-neutral-500 italic ml-8'>Testing Beta v0</h3>
                 <h1 className='text-6xl font-extralight'>What&#39;s New ?</h1>
-                <h1 className='mt-10'>Introducing orgneR beta, the web application designed to streamline and optimize your organization&#39;s structure. While orgneR is currently undergoing construction, it promises to revolutionize the way you manage your organization. Please note that certain features are temporarily disabled or under development, but the future holds a dynamic and powerful tool for enhancing your organizational efficiency .</h1>
+                <h1 className='mt-10'> We have scheduled maintenance for next update 12/20/2023 . </h1>
             </div>
         </div>
     )
